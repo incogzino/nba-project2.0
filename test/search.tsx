3f1e0f39df974fecd543fc1a57a871251318ff42
incogzino <jjnotm@gmail.com>
2024-04-04 14:34:59 +0100

    Jenkins file created

--- test/search.tsx
+++ test/search.tsx
@@ -1,22 +1,8 @@
-// import { render, fireEvent } from '@testing-library/react';
-// import App from '../src/searchList';
-
-// test('handleSearchInputChange updates searchQuery correctly', () => {
-//   const { getByPlaceholderText } = render(<App />);
-//   const searchInput = getByPlaceholderText('Search');
-
-//   fireEvent.change(searchInput, { target: { value: 'new input' } });
-
-//   expect(searchInput.value).toBe('new input');
-// });
-
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../src/App.tsx';
 
-
-
-// Mock the SearchList component
+// This mocks the SearchList component
 jest.mock('../src/App.tsx', () => ({ filteredPlayers }: { filteredPlayers: any[] }) => (
   <div data-testid="mock-search-list">
    
